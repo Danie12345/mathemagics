d5d34ea43990b151e9fa5ab574f5e5efe07ef139
Daniel Malo <danielmaloosorio@gmail.com>
2022-05-24 15:14:09 -0500

    Add styles to calculator

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -12,22 +12,28 @@ class Calculator extends React.Component {
   render() {
     return (
       <div className="calculator">
-        <div className="result"> </div>
-        <div className="operands">
-          <div className="appliers">
-            {this.appliers.map((el) => <button key={el} className="button" type="button">{el}</button>)}
+        <div className="result">0</div>
+        <div className="sections">
+          <div className="left">
+            <div className="appliers">
+              {this.appliers.map((el) => <button key={el} className="button" type="button">{el}</button>)}
+            </div>
+            <div className="operands">
+              <div className="numbers">
+                {[...Array(9).keys()].map((i) => <button key={i} className="button" type="button">{i + 1}</button>)}
+              </div>
+              <div className="pivots">
+                <button className="button x2" type="button">0</button>
+                <button className="button" type="button">.</button>
+              </div>
+            </div>
           </div>
-        </div>
-        <div>
-          {[...Array(9).keys()].map((i) => <button key={i} className="button" type="button">{i + 1}</button>)}
-          <div>
-            <button className="button x2" type="button">0</button>
-            <button className="button" type="button">.</button>
+          <div className="right">
+            <div className="operators" type="button">
+              {this.operators.map((el) => <button key={el} className="button" type="button">{el}</button>)}
+              <button className="button" type="button">=</button>
+            </div>
           </div>
-        </div>
-        <div className="operators" type="button">
-          {this.operators.map((el) => <button key={el} className="button" type="button">{el}</button>)}
-          <button className="button" type="button">=</button>
         </div>
       </div>
     );
