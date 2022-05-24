b42331907cbb6cd4b794734b42f04e23a205b5c6
Daniel Malo <danielmaloosorio@gmail.com>
2022-05-24 15:14:52 -0500

    Turn App into class

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import React from 'react';
 import './App.css';
 import Calculator from './components/Calculator';
 
-function App() {
-  return (
-    <Calculator name="Daniel" />
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <Calculator />
+    );
+  }
 }
 
 export default App;
