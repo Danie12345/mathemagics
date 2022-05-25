c052171b034f47420f9f96c14fe7a259a7fcca74
Daniel Malo <danielmaloosorio@gmail.com>
2022-05-25 12:47:27 -0500

    Transform Calculator.js
    Calculator class component is now a functional component with hooks.

--- src/components/calculator/Calculator.js
+++ src/components/calculator/Calculator.js
@@ -1,74 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Calculator.css';
 import calculate from './logic/calculate';
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: null,
-      operation: null,
-      next: null,
-    };
-    this.calculate = this.calculate.bind(this);
-    this.appliers = ['AC', '+/-', '%'];
-    this.operators = ['÷', '\u00d7', '-', '+', '='];
-  }
+const Calculator = () => {
+  const [screen, setScreen] = useState({ total: null, operation: null, next: null });
+  const appliers = ['AC', '+/-', '%'];
+  const operators = ['÷', '\u00d7', '-', '+', '='];
 
-  handleClick = (e) => {
-    this.calculate(e.target.innerHTML);
+  const recalculate = (btnName) => {
+    setScreen(calculate(screen, btnName));
   };
 
-  handleKeyDown = (e) => {
+  const handleClick = (e) => {
+    recalculate(e.target.innerHTML);
+  };
+
+  const handleKeyDown = (e) => {
     let btnName = e.key;
     if (!'.0123456789+-*/%=ac'.includes(btnName)) return;
     if (btnName === '*') btnName = '\u00d7';
     if (btnName === '/') btnName = '÷';
     if ('ac'.includes(btnName)) btnName = 'AC';
-    this.calculate(btnName);
+    recalculate(btnName);
   };
 
-  calculate = (btnName) => {
-    this.setState((state) => calculate(state, btnName));
-  }
-
-  render() {
-    const { total, operation, next } = this.state;
-    let pipedTotal;
-    let pipedNext;
-    if (total) pipedTotal = total.toString().substring(0, 8);
-    if (next) pipedNext = next.toString().substring(0, 8);
-    return (
-      <div className="calculator">
-        <div className="result">
-          { pipedTotal }
-          { operation }
-          { pipedNext }
-        </div>
-        <div className="sections">
-          <div className="left">
-            <div className="appliers">
-              {this.appliers.map((el) => <button key={el} onClick={this.handleClick} onKeyDown={this.handleKeyDown} className="button" type="button">{el}</button>)}
+  const { total, operation, next } = screen;
+  let pipedTotal;
+  let pipedNext;
+  if (total) pipedTotal = total.toString().substring(0, 8);
+  if (next) pipedNext = next.toString().substring(0, 8);
+  return (
+    <div className="calculator">
+      <div className="result">
+        { pipedTotal }
+        { operation }
+        { pipedNext }
+      </div>
+      <div className="sections">
+        <div className="left">
+          <div className="appliers">
+            {appliers.map((el) => <button key={el} onClick={handleClick} onKeyDown={handleKeyDown} className="button" type="button">{el}</button>)}
+          </div>
+          <div className="operands">
+            <div className="numbers">
+              {[...Array(9).keys()].map((i) => <button key={i} onClick={handleClick} onKeyDown={handleKeyDown} className="button" type="button">{i + 1}</button>)}
             </div>
-            <div className="operands">
-              <div className="numbers">
-                {[...Array(9).keys()].map((i) => <button key={i} onClick={this.handleClick} onKeyDown={this.handleKeyDown} className="button" type="button">{i + 1}</button>)}
-              </div>
-              <div className="pivots">
-                <button onClick={this.handleClick} onKeyDown={this.handleKeyDown} className="button x2" type="button">0</button>
-                <button onClick={this.handleClick} onKeyDown={this.handleKeyDown} className="button" type="button">.</button>
-              </div>
+            <div className="pivots">
+              <button onClick={handleClick} onKeyDown={handleKeyDown} className="button x2" type="button">0</button>
+              <button onClick={handleClick} onKeyDown={handleKeyDown} className="button" type="button">.</button>
             </div>
           </div>
-          <div className="right">
-            <div className="operators" type="button">
-              {this.operators.map((el) => <button key={el} onClick={this.handleClick} onKeyDown={this.handleKeyDown} className="button" type="button">{el}</button>)}
-            </div>
+        </div>
+        <div className="right">
+          <div className="operators" type="button">
+            {operators.map((el) => <button key={el} onClick={handleClick} onKeyDown={handleKeyDown} className="button" type="button">{el}</button>)}
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Calculator;
