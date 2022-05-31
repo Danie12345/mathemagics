b9aa2747406c233819e2d89bdfde42ed9f4295ea
Daniel Malo <danielmaloosorio@gmail.com>
2022-05-31 14:39:28 -0500

    Add calculator section content

---
+++ src/components/calculator-section/CalculatorSection.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Calculator from '../calculator/Calculator';
+
+const Home = () => {
+  return (
+    <div className='calculatorSection'>
+      <p>Let's do some nerdy maths!</p>
+      <Calculator />
+    </div>
+  );
+};
+
+export default Home;
