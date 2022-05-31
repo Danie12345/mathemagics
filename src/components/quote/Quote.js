47849028f4bb807cd7419a8c153809261bb3db39
Daniel Malo <danielmaloosorio@gmail.com>
2022-05-31 17:42:09 -0500

    Add quote styles

--- src/components/quote/Quote.js
+++ src/components/quote/Quote.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
+import './Quote.css';
+
 const Quote = () => (
   <div className="section quoteContainer">
     <p className="quote">
-      An equation means nothing to me unless it expresses a thought of God.
-      <small className="mathematician">- Srinivasa Ramanujan</small>
+      <span className="quote-content">
+        An equation means nothing to me unless it expresses a thought of God.
+      </span>
+      <small className="quote-mathematician">- Srinivasa Ramanujan</small>
     </p>
   </div>
 );
