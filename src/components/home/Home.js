1780e70d87b750d7c1b16c3a47e8a2e1d1186190
Daniel Malo <danielmaloosorio@gmail.com>
2022-05-31 17:21:21 -0500

    Configure specific selectors

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -4,8 +4,8 @@ import './Home.css';
 
 const Home = () => (
   <div className="section homeContainer">
-    <h2>Welcome to the Mathemagics website!</h2>
-    <p>
+    <h2 className="title">Welcome to the Mathemagics website!</h2>
+    <p className="introduction">
       Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam, in! Officia ex, corrupti
       error nemo doloribus labore ratione repudiandae laboriosam repellat neque asperiores, sint eos
       assumenda illum dicta voluptate. Dolorem temporibus veritatis facere explicabo doloremque
@@ -13,7 +13,7 @@ const Home = () => (
       deserunt aliquam dignissimos nisi minima perspiciatis officiis maiores nobis labore
       provident.
     </p>
-    <p>
+    <p className="introduction">
       Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam, in! Officia ex, corrupti
       error nemo doloribus labore ratione repudiandae laboriosam repellat neque asperiores, sint eos
       assumenda illum dicta voluptate. Dolorem temporibus veritatis facere explicabo doloremque
