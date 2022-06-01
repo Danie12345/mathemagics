dbeeea08ded9a5d036d51fe697a59ceb9f11d0c1
Daniel Malo <danielmaloosorio@gmail.com>
2022-06-01 16:05:34 -0500

    Add nav test
    Add snapshot

---
+++ src/__tests__/nav.test.js
@@ -0,0 +1,10 @@
+import renderer from 'react-test-renderer';
+import '@testing-library/jest-dom';
+import { BrowserRouter } from 'react-router-dom';
+
+const Nav = require('../components/nav/Nav.js').default;
+
+test('loads the container with everything', () => {
+  const tree = renderer.create(<BrowserRouter><Nav /></BrowserRouter>).toJSON();
+  expect(tree).toMatchSnapshot();
+})
