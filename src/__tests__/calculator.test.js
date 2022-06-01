14bc42ca8e1ade0ef0b80e580c52c98fbd396c0b
Daniel Malo <danielmaloosorio@gmail.com>
2022-06-01 14:50:11 -0500

    Update calculator.test.js

--- src/__tests__/calculator.test.js
+++ src/__tests__/calculator.test.js
@@ -1,8 +1,6 @@
 import renderer from 'react-test-renderer';
 import '@testing-library/jest-dom';
 
-import { Routes, Route, Navigate } from 'react-router-dom';
-
 const Calculator = require('../components/calculator/Calculator.js').default;
 
 test('loads the container with everything', () => {
