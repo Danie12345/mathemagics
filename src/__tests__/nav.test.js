d80d57e0f7a25916b181cf5e093fbe6a33061dea
Daniel Malo <danielmaloosorio@gmail.com>
2022-06-01 18:24:35 -0500

    Add user interaction for nav

--- src/__tests__/nav.test.js
+++ src/__tests__/nav.test.js
@@ -1,11 +1,19 @@
 import renderer from 'react-test-renderer';
 import '@testing-library/jest-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, MemoryRouter } from 'react-router-dom';
+import { render, screen, fireEvent } from '@testing-library/react';
 
 const Nav = require('../components/nav/Nav').default;
+const Container = require('../components/container/Container').default;
+const components = (<BrowserRouter><Nav /></BrowserRouter>);
 
 test('loads the nav with everything', () => {
-  const components = <BrowserRouter><Nav /></BrowserRouter>;
   const tree = renderer.create(components).toJSON();
   expect(tree).toMatchSnapshot();
 });
+
+test("selects a nav link", () => {
+  render(<MemoryRouter><Container/></MemoryRouter>);
+  fireEvent.click(screen.queryByText('Calculator'));
+  expect(screen.queryByText('Use the very awesome calculator!')).not.toBeNull();
+})
