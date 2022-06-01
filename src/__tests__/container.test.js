38515a794d54c642731c59604c58dd8577994228
Daniel Malo <danielmaloosorio@gmail.com>
2022-06-01 16:07:21 -0500

    Add container test
    Add snapshot

---
+++ src/__tests__/container.test.js
@@ -0,0 +1,10 @@
+import renderer from 'react-test-renderer';
+import '@testing-library/jest-dom';
+import { BrowserRouter } from 'react-router-dom';
+
+const Container = require('../components/container/Container.js').default;
+
+test('loads the container with everything', () => {
+  const tree = renderer.create(<BrowserRouter><Container /></BrowserRouter>).toJSON();
+  expect(tree).toMatchSnapshot();
+})
